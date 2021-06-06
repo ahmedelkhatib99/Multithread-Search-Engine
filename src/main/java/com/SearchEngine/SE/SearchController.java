1b34428f63a7eee5fc5a91792b0c6d4ab96f8ffb
Ahmed Abdulkader <49191231+ahmedabdulkader99@users.noreply.github.com>
2021-06-06 04:23:51 +0200

    Fix Cors Policy

--- src/main/java/com/SearchEngine/SE/SearchController.java
+++ src/main/java/com/SearchEngine/SE/SearchController.java
@@ -2,10 +2,7 @@ package com.SearchEngine.SE;
 
 import org.bson.Document;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -13,6 +10,7 @@ import java.util.ArrayList;
 @Controller
 public class SearchController {
 
+    @CrossOrigin
     @GetMapping("/api/search")
     @ResponseBody
     public ArrayList<SearchResult> searchDB(@RequestParam("query") String query, @RequestParam(value = "page", required = true, defaultValue = "1") int page) throws FileNotFoundException {
