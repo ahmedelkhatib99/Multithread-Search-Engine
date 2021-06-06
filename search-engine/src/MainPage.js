e6ac6c7c8e5238051272ea75bee88603d40ab438
Ahmed Abdulkader <49191231+ahmedabdulkader99@users.noreply.github.com>
2021-06-06 19:49:59 +0200

    Fix Logging in MongoDB

--- search-engine/src/MainPage.js
+++ search-engine/src/MainPage.js
@@ -1,46 +1,51 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import main_image from './main.JPG'
-import ResultPage from './ResultPage'
+import React, { Component } from 'react';
+import axios from 'axios';
+import main_image from './main.JPG';
+import ResultPage from './ResultPage';
 
-import './MainPage.css'
+import './MainPage.css';
 
 export class MainPage extends Component {
-    state={
-        "value": ''
-    }
+  state = {
+    value: '',
+  };
 
-    componentDidMount(){
-    }
+  componentDidMount() {}
 
-    _handleKeyDown = (e) => {
-        if (e.key === 'Enter') {
-            this.setState({
-                redirect: true,
-                value: e.target.value
-            })
-        }
+  _handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      this.setState({
+        redirect: true,
+        value: e.target.value,
+      });
     }
+  };
 
-    render() {
-        if(this.state.redirect){
-            return <ResultPage searchText={this.state.value}/>
-        }
-        return (
-            <div id="main-page-div" className="container-fluid">
-                <div id="main-row-div" className="row"></div>
-                <div id="main-row-div" className="row">
-                    <div className="row">
-                        <img src={main_image} alt="main pic"/>
-                    </div>
-                    <div className="row">
-                        <input id="search-bar" type="text" placeholder="Search" aria-label="Search" onKeyDown={this._handleKeyDown}></input>
-                    </div>
-                </div>
-                <div id="main-row-div" className="row"></div>
-            </div>
-        )
+  render() {
+    if (this.state.redirect) {
+      return <ResultPage searchText={this.state.value} />;
     }
+    return (
+      <div id='main-page-div' className='container-fluid'>
+        <div id='main-row-div' className='row'></div>
+        <div id='main-row-div' className='row'>
+          <div className='row'>
+            <img src={main_image} alt='main pic' />
+          </div>
+          <div className='row'>
+            <input
+              id='search-bar'
+              type='text'
+              placeholder='Search'
+              aria-label='Search'
+              onKeyDown={this._handleKeyDown}
+            ></input>
+          </div>
+        </div>
+        <div id='main-row-div' className='row'></div>
+      </div>
+    );
+  }
 }
 
-export default MainPage
+export default MainPage;
