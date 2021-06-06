deb39c41fa77b18fd702bdb3dff3ba91ea24931f
Ahmed Abdulkader <49191231+ahmedabdulkader99@users.noreply.github.com>
2021-06-06 04:23:59 +0200

    Connect DB to Front End Server

--- search-engine/src/ResultPage.js
+++ search-engine/src/ResultPage.js
@@ -1,72 +1,83 @@
-import React, { Component } from 'react'
-import ResultList from './ResultList'
-import axios from 'axios'
-import main_image from './main.JPG'
+import React, { Component } from 'react';
+import ResultList from './ResultList';
+import axios from 'axios';
+import main_image from './main.JPG';
 
-import './ResultPage.css'
+import './ResultPage.css';
 
 export class MainPage extends Component {
-    state={
-        "value": '',
-        "loading": true,
-        "results":[{
-           "url": "https://www.google.com.eg/",
-           "title": "Google",
-           "snippet": "search engine"
-        },
-        {
-            "url": "https://www.facebook.com/",
-            "title": "Facebook",
-            "snippet": "online social media"
-        }]
-    }
+  state = {
+    value: this.props.searchText,
+    loading: true,
+    results: [],
+  };
 
-    componentDidMount(){
-        this.setState({value: this.props.searchText})
-        this.getResults();
-    }
+  componentDidMount() {
+    this.setState({ results: [] });
+    this.getResults();
+  }
 
-    _handleKeyDown = (e) => {
-        if (e.key === 'Enter') {
-            this.setState({value: e.target.value})
-            this.getResults();
-        }
+  _handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      this.setState({ value: e.target.value });
+      this.setState({ results: [] });
+      this.getResults();
+      console.log(this.state.value);
     }
+  };
 
-    onInputchange = (e) => {
-        this.setState({value: e.target.value});
-    }
+  onInputchange = (e) => {
+    this.setState({ value: e.target.value });
+  };
 
-    getResults(){
-        //set results array in this function 
-        this.setState({loading: true});
-        axios.get('http://localhost:3000/')
-        .then(res => {
-            
-        }
-        )
-        .catch(error => {
-            alert(error.response.data.message);
-        })
-        //this.setState({results:})
-        this.setState({value:""})
-        this.setState({loading: false});
-    }
+  getResults() {
+    //set results array in this function
+    this.setState({ loading: true });
+    axios
+      .get('http://localhost:8080/api/search', {
+        params: { query: this.state.value },
+      })
+      .then((res) => {
+        console.log(res.data);
+        this.setState({ results: res.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    this.setState({ value: '' });
+    this.setState({ loading: false });
+  }
 
-    render() {
-        return (
-            <div className="container-fluid">
-                <div id="search-row-div" className="row">
-                    <input id="search-bar" type="text" value={this.state.value} onChange={this.onInputchange} placeholder="Search" aria-label="Search" onKeyDown={this._handleKeyDown}></input>
-                </div>
-                <div id="result-list-div" className="row">
-                    <hr></hr>
-                    <div class="loader" hidden={!this.state.loading}>Loading...</div>
-                    <ResultList results={this.state.results}/>
-                </div>
+  render() {
+    return (
+      <>
+        {this.state.loading ? (
+          <h1>loading</h1>
+        ) : (
+          <div className='container-fluid'>
+            <div id='search-row-div' className='row'>
+              <input
+                id='search-bar'
+                type='text'
+                value={this.state.value}
+                onChange={this.onInputchange}
+                placeholder='Search'
+                aria-label='Search'
+                onKeyDown={this._handleKeyDown}
+              ></input>
             </div>
-        )
-    }
+            <div id='result-list-div' className='row'>
+              <hr></hr>
+              <div class='loader' hidden={!this.state.loading}>
+                Loading...
+              </div>
+              <ResultList results={this.state.results} />
+            </div>
+          </div>
+        )}
+      </>
+    );
+  }
 }
 
-export default MainPage
+export default MainPage;
